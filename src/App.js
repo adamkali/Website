7d3c8ab69b8f30868154186ce84b98b2b8fcaa90
Adam Kalinowski <adamkali@outlook.com>
2021-09-15 08:12:51 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import SideNav from './components/SideNav/SideNav';
-import ScrollingHeader from './components/ScrollingHeader/ScrollingHeade';
+import ScrollingHeade from './components/ScrollingHeader/ScrollingHeade';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/Home';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <ScrollingHeader/>
+        <ScrollingHeade/>
         <SideNav/>
         <Switch>
           <Route path='/' exact component={Home}/>
--- src/components/ScrollingHeader/ScrollingHeade.js
+++ src/components/ScrollingHeader/ScrollingHeade.js
@@ -1,21 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import '../../static/site.css'
 
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementsByClassName("scrolling-header").style.fontSize = "30px";
-    } else {
-        document.getElementsByClassName("scrolling-header").style.fontSize = "90px";
-    }
-}
-
 function ScrollingHeade() {
-    window.onscroll = function() {scrollFunction()};
-    return (
-        <div className='scrolling-header'>
-            Accounts of the Occult
-        </div>
-    )
+  return (
+    <div className='scrolling-header'>
+      <Link to='/' className='header-text'>
+        Accounts of the Occult
+      </Link>
+    </div>
+  )
 }
 
 export default ScrollingHeade
